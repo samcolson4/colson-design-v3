0f02d6470354eadf1f113065c2728591cdfb0c90
samcolson4 <samcolson4@gmail.com>
2020-12-02 15:40:45 +0000

    Add main detail section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
     <span>
         <main-page></main-page>
+        <main-detail></main-detail>
     </span>
 </template>
 
 <script>
 import MainPage from '../components/MainPage.vue';
+import MainDetail from '../components/MainDetail.vue';
 
 export default {
     name: 'home',
     components: {
-        MainPage
+        MainPage,
+        MainDetail
     }
 };
 </script>
