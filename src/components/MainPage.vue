f5a7aafdb1fd7e8182605d9c2e8b931967c12c20
samcolson4 <samcolson4@gmail.com>
2020-12-02 15:59:20 +0000

    Update text

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,9 +1,10 @@
 <template>
     <v-container fluid fill-height class="main-page">
         <v-layout justify-center align-center column pa-5>
-            <div class="display-1 font-weight-bold blue darken-3 white--text text-xs-center" style="max-width: 25em; padding: 1em;">I decided to change careers and become a developer to be part of a team building something new, shaping a product that ultimately makes a difference to peoples' lives.</div>
+            <div class="display-1 font-weight-bold blue darken-3 white--text text-xs-center" style="max-width: 25em; padding: 1em;">I changed careers to become a developer because I want to be part of a team building something new.</div>
         </v-layout>
     </v-container>
+    
 </template>
 
 <script>
