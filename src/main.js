2ae7f753840a2cf3c281d71d49300392518d5396
samcolson4 <samcolson4@gmail.com>
2020-12-02 10:59:28 +0000

    Linted. Update app.vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
-import "@babel/polyfill";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill';
 
 Vue.config.productionTip = false;
 
@@ -12,4 +12,4 @@ new Vue({
   store,
   vuetify,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
