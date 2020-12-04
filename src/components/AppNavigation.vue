370039e7d9ac23a06347a3b41a7ea04bb393ffd9
samcolson4 <samcolson4@gmail.com>
2020-12-04 16:21:49 +0000

    Refactor

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,18 +1,17 @@
 <template>
   <span>
 
-    <v-toolbar app color="white" dark>
+    <v-toolbar app color="white" >
       <v-toolbar-side-icon
-        class="hidden-md-and-up"
-        @click="drawer = !drawer"
-      ></v-toolbar-side-icon>
+        class="hidden-md-and-up">
+        </v-toolbar-side-icon>
       <v-spacer class="hidden-md-and-up"></v-spacer>
       <v-toolbar-title class="site-title">{{ appTitle }}</v-toolbar-title>
       <v-spacer class="hidden-sm-and-down"></v-spacer>
       <v-btn flat class="hidden-sm-and-down" to="/">Home</v-btn>
       <v-btn flat class="hidden-sm-and-down" to="/projects">Projects </v-btn>
       <v-spacer class="hidden-sm-and-down"></v-spacer>
-      <v-btn flat class="hidden-sm-and-down" to="/github">GitHub</v-btn>
+      <v-btn flat class="hidden-sm-and-down" to="/github">GitHub </v-btn>
       <v-btn flat class="hidden-sm-and-down" to="/linkedin">LinkedIn</v-btn>
     </v-toolbar>
   </span>
