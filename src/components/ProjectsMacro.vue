2366e8d0b721b334ad39cbced15c1c4fb6a0e21f
samcolson4 <samcolson4@gmail.com>
2020-12-04 17:47:41 +0000

    Refactor projects

---
+++ src/components/ProjectsMacro.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container fluid fill-height class="projects-page">
+    <v-layout justify-center align-center column pa-5>
+      <div
+        class="display-1 font-weight-bold deep-orange lighten-1 white--text text-xs-center"
+        style="max-width: 25em; padding: 1em;"
+      >
+        I want to build things that matter.
+      </div>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'ProjectsMacro'
+};
+</script>
+
+<style scoped>
+.projects-page {
+  background: url('https://i.imgur.com/oBMBsul.jpg');
+  background-size: cover;
+  width: 100%;
+  height: 66%;
+}
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,27 +1,19 @@
 <template>
-  <v-container fluid fill-height class="main-page">
-    <v-layout justify-center align-center column pa-5>
-      <div
-        class="display-1 font-weight-bold deep-orange lighten-1 white--text text-xs-center"
-        style="max-width: 25em; padding: 1em;"
-      >
-        I want to build things that matter.
-      </div>
-    </v-layout>
-  </v-container>
+  <span>
+  <projects-macro></projects-macro>
+  <project-cards></project-cards>
+  </span>
 </template>
 
 <script>
+import ProjectsMacro from '../components/ProjectsMacro.vue';
+import ProjectCards from '../components/ProjectCards.vue';
+
 export default {
-  name: 'Projects'
+  name: 'Projects',
+  components: {
+    ProjectsMacro,
+    ProjectCards,
+  }
 };
 </script>
-
-<style scoped>
-.main-page {
-  background: url('https://i.imgur.com/oBMBsul.jpg');
-  background-size: cover;
-  width: 100%;
-  height: 85%;
-}
-</style>
