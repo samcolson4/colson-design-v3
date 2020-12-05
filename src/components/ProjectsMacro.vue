dbbb14b259e1c578beddef0b7403b24e0448abff
samcolson4 <samcolson4@gmail.com>
2020-12-05 15:42:26 +0000

    Add parallax effect

--- src/components/ProjectsMacro.vue
+++ src/components/ProjectsMacro.vue
@@ -1,14 +1,16 @@
 <template>
-  <v-container fluid fill-height class="projects-page">
-    <v-layout justify-center align-center column pa-5>
-      <div
-        class="display-1 font-weight-bold deep-orange lighten-1 white--text text-xs-center"
-        style="max-width: 25em; padding: 1em;"
-      >
-        I want to build things that matter.
-      </div>
-    </v-layout>
-  </v-container>
+  <v-parallax src="https://i.imgur.com/oBMBsul.jpg" alt="sunset" class="projects-page">
+    <v-container fluid fill-height>
+      <v-layout justify-center align-center column pa-5>
+        <div
+          class="display-1 font-weight-bold deep-orange lighten-1 white--text text-xs-center"
+          style="max-width: 25em; padding: 1em;"
+        >
+          I want to build things that matter.
+        </div>
+      </v-layout>
+    </v-container>
+  </v-parallax>
 </template>
 
 <script>
@@ -19,8 +21,6 @@ export default {
 
 <style scoped>
 .projects-page {
-  background: url('https://i.imgur.com/oBMBsul.jpg');
-  background-size: cover;
   width: 100%;
   height: 66vh;
 }
