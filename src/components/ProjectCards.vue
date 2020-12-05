947a6801586ba4728358258becbf50e9bad6b031
samcolson4 <samcolson4@gmail.com>
2020-12-05 06:25:26 +0000

    Add Acebook project view and components

--- src/components/ProjectCards.vue
+++ src/components/ProjectCards.vue
@@ -12,7 +12,7 @@
             <v-container fill-height fluid>
               <v-layout fill-height>
                 <v-flex xs12 align-end flexbox>
-                  <span class="headline white--text">KETO</span>
+                  <span class="headline white--text">Signposts AR</span>
                 </v-flex>
               </v-layout>
             </v-container>
@@ -20,7 +20,7 @@
 
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">Keto</h3>
+              <h3 class="headline mb-0">Signposts AR</h3>
               <div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
                 mauris felis, varius rutrum massa a, dignissim ornare dui. Cras
@@ -33,18 +33,20 @@
 
       <v-flex xs12 sm12 md4>
         <v-card>
-          <v-img src="http://source.unsplash.com/6S27S6pZ6o0" height="500px">
-            <v-container fill-height fluid>
-              <v-layout fill-height>
-                <v-flex xs12 align-end flexbox>
-                  <span class="headline white--text">PALEO</span>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-img>
+          <a class="card" href="/acebook">
+            <v-img class="card_image" src="http://source.unsplash.com/6S27S6pZ6o0" height="500px">
+              <v-container fill-height fluid>
+                <v-layout fill-height>
+                  <v-flex xs12 align-end flexbox>
+                    <span class="headline white--text">Acebook</span>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-img>
+          </a>
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">Paleo</h3>
+              <h3 class="headline mb-0">Acebook</h3>
               <div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
                 mauris felis, varius rutrum massa a, dignissim ornare dui. Cras
@@ -61,18 +63,16 @@
             <v-container fill-height fluid>
               <v-layout fill-height>
                 <v-flex xs12 align-end flexbox>
-                  <span class="headline white--text">VEGAN</span>
+                  <span class="headline white--text">Colson Design</span>
                 </v-flex>
               </v-layout>
             </v-container>
           </v-img>
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">Vegan</h3>
+              <h3 class="headline mb-0">Colson Design</h3>
               <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                mauris felis, varius rutrum massa a, dignissim ornare dui. Cras
-                eget velit eu dui tristique lobortis sit amet vel tellus.
+                This website is built in Vue JS, using Vuetify. You can view the project's GitHub page here.
               </div>
             </div>
           </v-card-title>
@@ -88,4 +88,12 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.card {
+  text-decoration: none;
+}
+
+.card .card_image :hover {
+  /* box-shadow: 3px 3px; */
+}
+</style>
---
+++ src/views/Acebook.vue
@@ -0,0 +1,21 @@
+<template>
+  <span>
+    <acebook-detail></acebook-detail>
+    <br>
+    <hr>
+    <project-cards></project-cards>
+  </span>
+</template>
+
+<script>
+import AcebookDetail from '../components/AcebookDetail.vue';
+import ProjectCards from '../components/ProjectCards.vue';
+
+export default {
+  name: 'Acebook',
+  components: {
+    ProjectCards,
+    AcebookDetail
+  }
+};
+</script>
