23e5a5b0c70e468a50420db3950a79f7db39c82d
samcolson4 <samcolson4@gmail.com>
2020-12-07 09:14:09 +0000

    Remove site header

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,22 +1,15 @@
 <template>
-  <span>
-    <v-app-bar fixed app elevate-on-scroll hide-on-scroll color="white">
-      <v-toolbar-side-icon class="hidden-md-and-up"> </v-toolbar-side-icon>
-      <v-spacer class="hidden-sm-and-down"></v-spacer>
+    <v-app-bar fixed app elevate-on-scroll hide-on-scroll color="white">      
+      
       <v-tabs>
         <v-tab to="/">Home</v-tab>
         <v-tab to="/projects">Projects</v-tab>
         <v-tab to="/cv">CV</v-tab>
       </v-tabs>
-            
-      <v-toolbar-title>
-      <a id="app_title" href="/">{{ appTitle }}</a>
-      </v-toolbar-title>
-   
+  
       <v-btn flat class="hidden-sm-and-down" to="/github"> GitHub </v-btn>
       <v-btn flat class="hidden-sm-and-down" to="/linkedin"> LinkedIn </v-btn>
     </v-app-bar>
-  </span>
 </template>
 
 <script>
@@ -31,7 +24,6 @@ export default {
 </script>
 
 <style scoped>
-
 a {
   text-decoration: none;
   color: black;
