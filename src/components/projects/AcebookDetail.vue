5f4f90a75de3a53b4b35e5428f809568d8a802a6
samcolson4 <samcolson4@gmail.com>
2020-12-09 09:01:59 +0000

    Update Acebook detail view

--- src/components/projects/AcebookDetail.vue
+++ src/components/projects/AcebookDetail.vue
@@ -9,15 +9,17 @@
       <v-container fluid fill-height>
         <v-layout justify-center align-center column pa-5>
           <div
-            class="display-1 font-weight-bold green darken-1 white--text text-xs-center"
+            class="display-1 font-weight-bold white--text text-xs-center" :class="project.color"
             style="max-width: 25em; padding: 1em;"
-          >
+          >                {{ project.title }}
+
           </div>
         </v-layout>
       </v-container>
     </v-parallax>
 
-                {{ project.title }}
+    {{ project.intro }}
+
 
   </div>
   </v-container>
