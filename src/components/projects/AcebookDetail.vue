a0ec97cb2d6351013f7b7e4b9be82c28a98b770e
samcolson4 <samcolson4@gmail.com>
2020-12-09 11:01:16 +0000

    Centralise content

--- src/components/projects/AcebookDetail.vue
+++ src/components/projects/AcebookDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <div v-for="project in projects" v-bind:key="project">
-      <v-parallax :src="project.par_img" alt="acebook screenshot">
+      <v-parallax :src="project.par_img" alt="person at laptop">
         <v-container fluid fill-height>
           <v-layout justify-center align-center column pa-5>
             <div
@@ -14,10 +14,21 @@
           </v-layout>
         </v-container>
       </v-parallax>
-      <v-card>
-        {{ project.intro }}
-      </v-card>
-
+      <v-row class="justify-center">
+        <v-column>
+          <v-card max-width="30em" class="ma-5">
+            <v-card-title> About {{project.title}} </v-card-title>
+            <v-card-text>
+            {{ project.intro }}
+            </v-card-text>
+          </v-card>
+        </v-column>
+        <v-column>
+          <v-card class="ma-5">
+          <v-img :src="project.logo"></v-img>
+          </v-card>
+        </v-column>
+      </v-row>
       <v-parallax :src="project.par_img" alt="acebook screenshot"></v-parallax>
     </div>
   </v-container>
