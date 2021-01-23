8cc88d89123d376f7c857ef98dc30bc8f684aad7
samcolson4 <samcolson4@gmail.com>
2021-01-23 11:34:43 +0000

    Add contact section

---
+++ src/components/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-layout column>
+      <v-card>
+        <v-card-title>
+          Contact
+        </v-card-title>
+        <v-card-text>
+          I am currently freelance, looking for a permanent position as a software engineer. If you'd like to get in touch about this, or anything else, please get in touch.
+        </v-card-text>
+        
+        <v-btn
+              href="mailto:samcolson4@gmail.com"
+              class="ma-3 white--text blue"
+              >Get in touch</v-btn
+            >
+
+      </v-card>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Contact'
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <span>
     <main-page></main-page>
     <main-detail></main-detail>
+    <contact></contact>
   </span>
 </template>
 
 <script>
 import MainPage from '../components/MainPage.vue';
 import MainDetail from '../components/MainDetail.vue';
+import Contact from '../components/Contact.vue';
 
 export default {
   name: 'home',
   components: {
     MainPage,
-    MainDetail
+    MainDetail,
+    Contact
   }
 };
 </script>
